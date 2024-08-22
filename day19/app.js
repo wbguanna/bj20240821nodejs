4786a12828118ea0421b78f1c569ee435ea6b169
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-08-22 13:14:59 +0900

    Login 페이지 로그인폼, app.js 로그인 api 추가

--- day19/app.js
+++ day19/app.js
@@ -2,6 +2,7 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const path = require("path");
+// const bodyParser = require("body-parser");
 
 app.set("port", 3000);
 app.set("views", path.join(__dirname, "views")); // 절대경로 // 작업디렉토리가 바뀐다면 이게 나을 수 있음
@@ -9,6 +10,43 @@ app.set("views", "views"); // 상대경로 // 보안?
 app.set("view engine", "ejs");
 
 app.use(express.static("public")); // public 폴더 정적 제공
+
+// 4.16.1 버전 이후로 bodyParser가 내장되어있다함..
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const memberList = [
+  {
+    no: 101,
+    id: "user01",
+    password: "1234",
+    name: "홍길동",
+    email: "hong@gmail.com",
+  },
+  {
+    no: 102,
+    id: "user01",
+    password: "1234",
+    name: "홍길동",
+    email: "hong@gmail.com",
+  },
+  {
+    no: 103,
+    id: "user01",
+    password: "1234",
+    name: "홍길동",
+    email: "hong@gmail.com",
+  },
+  {
+    no: 104,
+    id: "user01",
+    password: "1234",
+    name: "홍길동",
+    email: "hong@gmail.com",
+  },
+];
+
+let noCnt = 105;
 
 app.get("/home", (req, res) => {
   req.app.render("home/Home", {}, (err, html) => {
@@ -33,6 +71,39 @@ app.get("/profile", (req, res) => {
 });
 app.get("/shop", (req, res) => {
   req.app.render("shop/Shop", {}, (err, html) => {
+    res.end(html);
+  });
+});
+
+app.get("/login", (req, res) => {
+  req.app.render("member/Login", {}, (err, html) => {
+    res.end(html);
+  });
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  //todo: 검증 넣기
+
+  const idx = memberList.findIndex((member) => member.id === req.body.id);
+  if (idx !== -1) {
+    if (memberList[idx].password === req.body.password)
+      console.log("로그인 성공");
+    // 로그인 성공 확인후 세션에 로그인정보를 저장하게
+    else console.log("로그인 실패!");
+  }
+  req.app.render("member/Login", {}, (err, html) => {
+    res.end(html);
+  });
+});
+app.get("/join", (req, res) => {
+  req.app.render("member/Joinus", {}, (err, html) => {
+    res.end(html);
+  });
+});
+
+app.post("/join", (req, res) => {
+  req.app.render("member/Joinus", {}, (err, html) => {
     res.end(html);
   });
 });
