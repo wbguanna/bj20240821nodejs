c7909c20818fa25fa620305fa5f90716f09ef7f5
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-08-22 13:48:47 +0900

    app.js 미들웨어 에러 핸들링 추가

--- day19/app.js
+++ day19/app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
 const router = express.Router();
+const cookieParser = require("cookie-parser");
 
 app.set("port", 3000);
 app.set("views", path.join(__dirname, "views")); // 절대경로 // 작업디렉토리가 바뀐다면 이게 나을 수 있음
@@ -17,6 +18,8 @@ app.use(express.static("public")); // public 폴더 정적 제공
 // app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// 쿠키 사용 미들웨어 설정
+app.use(cookieParser());
 
 const memberList = [
   {
@@ -62,11 +65,6 @@ router.route("/gallery").get((req, res) => {
     res.end(html);
   });
 });
-router.route("/member").get((req, res) => {
-  req.app.render("member/Member", {}, (err, html) => {
-    res.end(html);
-  });
-});
 router.route("/profile").get((req, res) => {
   req.app.render("profile/Profile", {}, (err, html) => {
     res.end(html);
@@ -78,8 +76,21 @@ router.route("/shop").get((req, res) => {
   });
 });
 
+router.route("/member").get((req, res) => {
+  req.app.render("member/Member", {}, (err, html) => {
+    res.end(html);
+  });
+});
+
 router.route("/login").get((req, res) => {
   req.app.render("member/Login", {}, (err, html) => {
+    // TODO: 클라에서 받은거 저장시켜 전달하기
+    // 사용자(접속자)의 로컬에 쿠키가 저장된다.
+    res.cookie("user", {
+      id: "TestUser",
+      name: "테스트 유저",
+      authorized: true,
+    });
     res.end(html);
   });
 });
@@ -113,6 +124,23 @@ router.route("/join").post((req, res) => {
 });
 
 app.use("/", router);
+
+// 라우터 설정이 끝나고 나서 들어가야 됨
+// 등록되지 않은 패스에 대해
+app.use("*", (req, res) => {
+  res.status(404).send(`<h1>Error - 페이지를 찾을 수 없습니다</h1>`);
+});
+
+const expressErrorHandler = require("express-error-handler");
+
+const errorHandler = expressErrorHandler({
+  static: {
+    404: ["./public/404.html", "./public/resources/images/img404.PNG"],
+  },
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
 
 const server = http.createServer(app);
 server.listen(app.get("port"), () => {
