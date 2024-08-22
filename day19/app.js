26ab7864bcd089fa96873d59bf82e198238cf25c
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-08-22 10:11:52 +0900

    day19 nodejs-ejs 프로젝트 셋팅

---
+++ day19/app.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("port", 3000);
+app.set("views", path.join(__dirname, "views")); // 절대경로 // 작업디렉토리가 바뀐다면 이게 나을 수 있음
+app.set("views", "views"); // 상대경로 // 보안?
+app.set("view engine", "ejs");
+
+app.use(express.static("public")); // public 폴더 정적 제공
+
+app.get("/hong", (req, res) => {
+  req.app.render("Home", {}, (err, html) => {
+    res.end(html);
+  });
+});
+
+const server = http.createServer(app);
+server.listen(app.get("port"), () => {
+  console.log(``); // 최신문법 쓸때는 호환이 되ㅡㄴ지 확인해보자
+});
