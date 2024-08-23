35bb7bfa46928f08daced255cb2fcabd540a976a
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-08-23 14:26:16 +0900

    day20 app.js 강사님 코드로 변경 (feat. nodemon-ejs 캐시 뻘짓)

--- day20/app.js
+++ day20/app.js
@@ -1,28 +1,25 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-const path = require("path");
 const bodyParser = require("body-parser");
-const router = express.Router();
 const cookieParser = require("cookie-parser");
 const expressSession = require("express-session");
-const multer = require("multer");
+// 파일 업로드용 미들웨어
+var multer = require("multer");
+var fs = require("fs");
 
 app.set("port", 3000);
-// app.set("views", path.join(__dirname, "views")); // 절대경로 // 작업디렉토리가 바뀐다면 이게 나을 수 있음
-app.set("views", "views"); // 상대경로 // 보안?
+app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.use(express.static("public")); // public 폴더 정적 제공
-app.use("./uploads", express.static("uploads")); // public 폴더 정적 제공
-
-// 4.16.1 버전 이후로 bodyParser가 내장되어있다함..
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
+// POST 방식으로 파라미터 전달 받기 위한 설정
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // 쿠키 사용 미들웨어 설정
 app.use(cookieParser());
+// 세션 미들웨어 등록
 app.use(
   expressSession({
     secret: "my key",
@@ -31,33 +28,6 @@ app.use(
   })
 );
 
-// 파일 업로드를 위한 storage 설정(업로드 폴더, 파일명 등)
-var storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, "uploads");
-  },
-  filename: function (req, file, callback) {
-    // 파일명 중복을 방지하기 위한 처리
-    // Date.now() <-- 타임스템프
-    const fileName = Buffer.from(file.originalname, "latin1").toString("utf8");
-    let index = fileName.lastIndexOf(".");
-    let newFileName = fileName.substring(0, index);
-    newFileName += Date.now();
-    newFileName += fileName.substring(index);
-    callback(null, newFileName);
-  },
-});
-// 파일 제한: 10개, 1G 이하
-var upload = multer({
-  storage: storage,
-  limits: {
-    files: 10,
-    fileSize: 1024 * 1024 * 1024,
-  },
-});
-
-let cnt = 1;
-console.log(`되나? ${cnt++}}`);
 // 임시 데이터
 const memberList = [
   {
@@ -69,34 +39,30 @@ const memberList = [
   },
   {
     no: 102,
-    id: "user01",
-    password: "1234",
-    name: "홍길동",
-    email: "hong@gmail.com",
+    id: "user02",
+    password: "12345",
+    name: "김길동",
+    email: "kim@gmail.com",
   },
   {
     no: 103,
-    id: "user01",
-    password: "1234",
-    name: "홍길동",
-    email: "hong@gmail.com",
+    id: "user03",
+    password: "123",
+    name: "박길동",
+    email: "lee@gmail.com",
   },
   {
     no: 104,
-    id: "user01",
-    password: "1234",
-    name: "홍길동",
-    email: "hong@gmail.com",
+    id: "user04",
+    password: "123456",
+    name: "이길동",
+    email: "park@gmail.com",
   },
 ];
 let noCnt = 105;
 
 // 쇼핑몰 상품 목록
-
-// 몽고 db 에서는 primary key를 id // id는 숫자이더라도 문자열로 두긴한다.
-// row 라고 안하고 document라고 한다..
-// 여기서 carList 는 컬렉션이라고 말한다..
-// 물리적 단계? 논리적 단계로 넘어가서 쓸 수 있다..
+// TODO:날짜형식은?
 const carList = [
   {
     _id: 111,
@@ -104,44 +70,30 @@ const carList = [
     price: 3000,
     year: 1999,
     company: "SAMSUNG",
-    img: "",
+    writedate: "",
+    photos: [
+      {
+        originalname: "르노삼성sm520.png",
+        filename: "르노삼성sm520.png",
+        filesize: 371000,
+        mimetype: "img/png",
+      },
+      {
+        originalname: "르노삼성sm5.png",
+        filename: "르노삼성sm5.png",
+        filesize: 95900,
+        mimetype: "img/png",
+      },
+    ],
   },
-  { _id: 112, name: "SM7", price: 5000, year: 2013, company: "SAMSUNG" },
-  { _id: 113, name: "SONATA", price: 3000, year: 2023, company: "SAMSUNG" },
-  { _id: 114, name: "GRANDEUR", price: 4000, year: 2022, company: "HYUNDAI" },
-  { _id: 115, name: "BMW", price: 6000, year: 2019, company: "BMW" },
-  { _id: 116, name: "SONATA", price: 3000, year: 2023, company: "SAMSUNG" },
 ];
-let carCnt = 117;
+let carSeq = 117;
 
-console.log(`되나? ${cnt++}}`);
-
-//뷰 엔진 없이 fs 모듈을 이용한 뷰 렌더링
-// router.route("/text").get(function (req, res) {
-//   console.log("test ejs");
-//   fs.readFile("views/test.ejs", "utf8", function (err, data) {
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     res.end(ejs.render(data));
-//   });
-// });
+// 요청 라운팅 사용
+const router = express.Router();
 
 router.route("/home").get((req, res) => {
   req.app.render("home/Home", {}, (err, html) => {
-    if (err) {
-      console.error(err);
-      // res.status(500).send("Error rendering the page");
-      res.status(500).send(JSON.stringify(err));
-      console.log(JSON.stringify(err));
-      const test = JSON.stringify(err);
-      console.log(test);
-    } else {
-      res.end(html);
-    }
-  });
-});
-
-router.route("/gallery").get((req, res) => {
-  req.app.render("gallery/Gallery", {}, (err, html) => {
     res.end(html);
   });
 });
@@ -150,36 +102,21 @@ router.route("/profile").get((req, res) => {
     res.end(html);
   });
 });
-
-// 예시 uri 상품정보 라우팅
-// router.route("/process/product").get((req, res) => {
-//   console.log("/process/product 호출.");
-
-//   if (req.session.user === undefined) {
-//     res.redirect("/public/login2.html");
-//   } else {
-//     res.redirect("/public/product.html");
-//   }
-// });
-
 router.route("/member").get((req, res) => {
-  // 로그인이 되어있다면 member 페이지를 보여준다.
+  // 로그인이 되어 있다면 member 페이지를 보여준다.
   // 쿠키는 사용자쪽에 전달(res), 세션은 요청 들어올때 생성(req)
   if (req.session.user !== undefined) {
     const user = req.session.user;
-    // TODO: 로그인이 안된상태 처리
     req.app.render("member/Member", { user }, (err, html) => {
-      // 렌더링시 객체 전달
       res.end(html);
     });
   } else {
     res.redirect("/login");
   }
 });
-
 router.route("/login").get((req, res) => {
   req.app.render("member/Login", {}, (err, html) => {
-    // 사용자(접속자)의 로컬에 쿠키가 저장된다.
+    // 사용자(접속자)의 로컬에 쿠키가 저장 된다.
     res.cookie("user", {
       id: "TestUser",
       name: "테스트 유저",
@@ -188,18 +125,13 @@ router.route("/login").get((req, res) => {
     res.end(html);
   });
 });
-
 router.route("/login").post((req, res) => {
-  // console.log(req.body);
-  //TODO: 검증 넣기
-
-  // TODO: 클라에서 받은거 저장시켜 전달하기
+  console.log(req.body.id, req.body.password);
   const idx = memberList.findIndex((member) => member.id === req.body.id);
-  if (idx !== -1) {
+  if (idx != -1) {
     if (memberList[idx].password === req.body.password) {
-      console.log("로그인 성공");
-      // 로그인 성공 확인후 세션에 로그인정보를 저장하게
-      // 세션에 로그인 정보 등록후 메인 페이지로 이동
+      console.log("로그인 성공!");
+      // 세션에 로그인 정보를 등록 후 멤버 페이지 이동
       req.session.user = {
         id: req.body.id,
         name: memberList[idx].name,
@@ -208,51 +140,30 @@ router.route("/login").post((req, res) => {
       };
       res.redirect("/member");
     } else {
-      console.log("로그인 실패!");
+      console.log("로그인 실패! 패스워드가 맞지 않습니다.");
+      // 다시 로그인 페이지로 다시 이동
       res.redirect("/login");
     }
   } else {
     console.log("존재하지 않는 계정입니다.");
     res.redirect("/login");
   }
-  // req.app.render("member/Login", {}, (err, html) => {
-  //   res.end(html);
-  // });
 });
-
 router.route("/logout").get((req, res) => {
-  console.log("/process/logout 호출됨.");
-  //로그인 된 상태
+  console.log("GET - /logout 호출 ...");
+  // 로그인 된 상태라면 로그아웃
   if (!req.session.user) {
     console.log("아직 로그인 전 상태입니다.");
     res.redirect("/login");
     return;
   }
-
-  // 세션의 user 정보를 제거해서 logout 처리
+  // 세션의 user 정보를 제거 해서 logout처리
   req.session.destroy((err) => {
     if (err) throw err;
     console.log("로그아웃 성공!");
     res.redirect("/login");
   });
 });
-
-// Promise 방식
-// router.route("/logout").get(async (req, res) => {
-//   console.log("/process/logout 호출됨.");
-//   if (req.session.user) {
-//     //로그인 된 상태
-//     if (!req.session.user) {
-//       console.log("아직 로그인 전 상태입니다.");
-//       res.redirect("/login");
-//       return;
-//     }
-//   }
-
-//   // 세션의 user 정보를 제거해서 logout 처리
-//   await req.session.destroy();
-// });
-
 router.route("/joinus").get((req, res) => {
   // 회원 가입 ejs 페이지 forward
   req.app.render("member/Joinus", {}, (err, html) => {
@@ -263,25 +174,45 @@ router.route("/joinus").post((req, res) => {
   // 회원 가입 처리 후 목록으로 갱신
   res.redirect("/member");
 });
-
-// ----- 쇼핑몰 기능
-router.route("/shop").get((req, res) => {
-  req.app.render("shop/Shop", {}, (err, html) => {
+router.route("/gallery").get((req, res) => {
+  req.app.render("gallery/Gallery", {}, (err, html) => {
     res.end(html);
   });
 });
-
+// ---- 쇼핑몰 기능
+router.route("/shop").get((req, res) => {
+  req.app.render("shop/Shop", { carList }, (err, html) => {
+    if (err) throw err;
+    res.end(html);
+  });
+});
 router.route("/shop/insert").get((req, res) => {
   req.app.render("shop/Insert", {}, (err, html) => {
     res.end(html);
   });
 });
-
+router.route("/shop/insert").post(upload.array("photo", 1), (req, res) => {
+  console.log("POST - /shop/insert");
+  const { name, price, year, company } = req.body;
+  const newCar = {
+    _id: carSeq++,
+    name,
+    price,
+    year,
+    company,
+    writedate: Date.now(),
+    photos: [],
+  };
+  newCar.photos = req.files;
+  carList.push(newCar);
+  ///res.send(carList);
+  res.redirect("/shop");
+});
 router.route("/shop/modify").get((req, res) => {
   const _id = parseInt(req.query._id);
   console.log(_id);
-  const idx = carList.findIndex((car) => car._id === _id);
-  console.dir(idx);
+  const idx = carList.findIndex((car) => _id === car._id);
+  console.log(idx);
   if (idx === -1) {
     console.log("상품이 존재 하지 않습니다.");
     res.redirect("/shop");
@@ -297,18 +228,19 @@ router.route("/shop/modify").post((req, res) => {
   console.dir(req.body);
   res.redirect("/shop");
 });
-
 router.route("/shop/detail").get((req, res) => {
   // 쿼리로 전송된 데이터는 모두 문자열이다.
-  // parseInt() 필수 "56" <-- 이런 문자열 숫자를 numeric 이라 한다..
-  const _id = req.query._id;
+  // parseInt() 필수 "56" <-- numeric
+  const _id = parseInt(req.query._id);
+  //console.log(_id)
   const idx = carList.findIndex((car) => _id === car._id);
+  //console.log(idx);
   if (idx === -1) {
     console.log("상품이 존재 하지 않습니다.");
     res.redirect("/shop");
     return;
   }
-  req.app.render("/shop/Detail", { car: carList[idx] }, (err, html) => {
+  req.app.render("shop/Detail", { car: carList[idx] }, (err, html) => {
     if (err) throw err;
     res.end(html);
   });
@@ -324,26 +256,26 @@ router.route("/shop/cart").get((req, res) => {
   });
 });
 
+// router 설정 맨 아래에 미들웨어 등록
 app.use("/", router);
 
-// 라우터 설정이 끝나고 나서 들어가야 됨
 // 등록되지 않은 패스에 대해 페이지 오류 응답
-// app.use("*", (req, res) => {
-//   res.status(404).send(`<h1>Error - 페이지를 찾을 수 없습니다</h1>`);
+// app.all('*', function(req, res) {
+//     res.status(404).send('<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>')
 // });
 
 const expressErrorHandler = require("express-error-handler");
-
+//모든 라우터 처리 후 404 오류 페이지 처리
 const errorHandler = expressErrorHandler({
   static: {
     404: "./public/404.html",
   },
 });
-
 app.use(expressErrorHandler.httpError(404));
 app.use(errorHandler);
 
+// 서버 생성 및 실행
 const server = http.createServer(app);
 server.listen(app.get("port"), () => {
-  console.log(`server start: http://localhost:${app.get("port")}`); // 최신문법 쓸때는 호환이 되ㅡㄴ지 확인해보자
+  console.log(`Run on server >>> http://localhost:${app.get("port")}`);
 });
